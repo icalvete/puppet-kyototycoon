732c9fb0692033c50bdb6bea595cff4ac907806f
Israel Calvete <icalvete@gmail.com>
2018-04-26 09:21:05 +0200

    Update service.

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,11 +1,12 @@
 class kyototycoon::service {
 
   service{ 'ktserver':
-    ensure     => running,
-    enable     => true,
-    hasstatus  => false,
-    hasrestart => true,
-    provider   => 'base'
+    ensure    => running,
+    provider  => 'base',
+    hasstatus => false,
+    start     => "/etc/init.d/ktserver start",
+    stop      => "/etc/init.d/ktserver stop",
+    pattern   => "/usr/bin/ktserver",
   }
 }
 
